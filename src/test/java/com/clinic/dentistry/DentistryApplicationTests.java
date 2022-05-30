4aee714f3225f0d9affd3f8ba319efc946c18c64
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-05-30 23:21:32 +0300

    Initial commit

---
+++ src/test/java/com/clinic/dentistry/DentistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.dentistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
