6ef1972521a91ad2285d8de0e944d251db333913
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-09-06 17:12:57 +0300

    Fixed bugs

--- src/main/resources/db/Add_admin.sql
+++ src/main/resources/db/Add_admin.sql
@@ -1,5 +1,5 @@
 insert into usr (id, username, password, active)
-values (1, 'admin', '$2a$08$REVL9IX0UmZZJHP4zXxix.IrMws8ELlYUbmdqbv9AYnYeZoNef.SO', true);
+values (1, 'admin', 'admin', true);
 
 insert into user_role (user_id, roles)
 values (1, 'ADMIN');
