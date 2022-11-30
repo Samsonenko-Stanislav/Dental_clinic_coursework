7495d6bb5bc5d994602f902b012744c9d026a7a5
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-11-30 20:08:44 +0300

    Some code refactoring

--- src/main/java/com/clinic/dentistry/models/User.java
+++ src/main/java/com/clinic/dentistry/models/User.java
@@ -1,5 +1,6 @@
 package com.clinic.dentistry.models;
 
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,6 +9,7 @@ import java.util.Collection;
 import java.util.Set;
 
 @Entity
+@Data
 @Table(name = "usr")
 public class  User implements UserDetails {
     @Id
@@ -34,131 +36,29 @@ public class  User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    public String getFullName() {
-        String fullName = "";
-        if (employee != null){
-            fullName = employee.getFullName();
-        } else if (outpatientCard != null) {
-            fullName = outpatientCard.getFullName();
-        }
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        if (employee != null){
-            employee.setFullName(fullName);
-        }
-        if (outpatientCard != null) {
-            outpatientCard.setFullName(fullName);
-        }
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return getRoles();
-    }
-
-    public String getPassword() {
-        return password;
+        return null;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isEnabled() {
-        return isActive();
+        return false;
     }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    public OutpatientCard getOutpatientCard() {
-        return outpatientCard;
-    }
-
-    public void setOutpatientCard(OutpatientCard outpatientCard) {
-        this.outpatientCard = outpatientCard;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public void setGender(Gender gender){
-        this.gender = gender;
-    }
-
-    public String getEmail(){
-        return  email != null ? email : "None";
-    }
-
-    public Gender getGender(){
-        return gender != null ? outpatientCard.getGender() : Gender.NONE;
-    }
-
-    public String employeeJobTitle(){
-        return employee.getJobTitle();
-    }
-    public Long getEmployeeId(){
-        return employee != null ? employee.getId() : 0;
-    }
-
-    public Long getOutpatientCardId(){
-        return outpatientCard != null ? outpatientCard.getId() : 0;
-    }
-
-
 }
