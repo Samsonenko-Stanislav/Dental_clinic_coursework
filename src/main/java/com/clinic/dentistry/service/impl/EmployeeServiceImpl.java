eddbbf84b30b6f3c5fde3988abe0ddd2fad0939e
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-12-02 01:58:23 +0300

    Refactored EmployeeController

---
+++ src/main/java/com/clinic/dentistry/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.clinic.dentistry.service.impl;
+
+import com.clinic.dentistry.models.Employee;
+import com.clinic.dentistry.repo.EmployeeRepository;
+import com.clinic.dentistry.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee){
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void editEmployee(Employee employee, Employee employee_new){
+        employee.setFullName(employee_new.getFullName());
+        employee.setJobTitle(employee_new.getJobTitle());
+        employee.setWorkStart(employee_new.getWorkStart());
+        employee.setWorkEnd(employee_new.getWorkEnd());
+        employee.setDurationApp(employee_new.getDurationApp());
+        employeeRepository.save(employee);
+    }
+}
