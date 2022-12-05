834cf7a78ccca0eda4d4c9eaacfed05499b99d87
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2022-12-05 14:21:06 +0300

    Added function for user's password changing

--- src/main/java/com/clinic/dentistry/service/impl/OutpatientCardServiceImpl.java
+++ src/main/java/com/clinic/dentistry/service/impl/OutpatientCardServiceImpl.java
@@ -4,9 +4,12 @@ import com.clinic.dentistry.models.Gender;
 import com.clinic.dentistry.models.OutpatientCard;
 import com.clinic.dentistry.models.User;
 import com.clinic.dentistry.repo.OutpatientCardRepository;
+import com.clinic.dentistry.repo.UserRepository;
 import com.clinic.dentistry.service.OutpatientCardService;
+import com.fasterxml.jackson.core.Base64Variant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +19,10 @@ import java.util.Map;
 public class OutpatientCardServiceImpl implements OutpatientCardService {
     @Autowired
     private OutpatientCardRepository outpatientCardRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository usesRepositotory;
 
     @Override
     public List<OutpatientCard> findAllCards(){
@@ -27,6 +34,9 @@ public class OutpatientCardServiceImpl implements OutpatientCardService {
         user.getOutpatientCard().setFullName(form.get("fullName"));
         user.getOutpatientCard().setEmail(form.get("email"));
         user.setUsername(form.get("username"));
+        if (form.get("changePassword").equals("on")){
+            user.setPassword(passwordEncoder.encode(form.get("password")));
+        }
         //TODO Понять почему не меняется пол, а точнее почему не находит MALE и FEMALE
         if (form.get("MALE") != null){
             user.getOutpatientCard().setGender(Gender.MALE);
@@ -35,5 +45,6 @@ public class OutpatientCardServiceImpl implements OutpatientCardService {
             user.getOutpatientCard().setGender(Gender.FEMALE);
         }
         outpatientCardRepository.save(user.getOutpatientCard());
+        usesRepositotory.save(user);
     }
 }
