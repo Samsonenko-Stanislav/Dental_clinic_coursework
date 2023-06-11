b0ac5afbf9b7657ebdc345c723c3d267367f1b54
Stanislav Samsonenko <stanislav@gmail.com>
2023-06-11 16:05:48 +0600

    Added Permission

--- client/front-end/src/App.js
+++ client/front-end/src/App.js
@@ -30,7 +30,7 @@ const App = () => {
 
   const isUser = user ? user?.role === 'user' : null;
   const isAdmin = user ? user?.role === 'admin' : null;
-  const isDoctor = user ? user?.role === 'doctor' : null;
+    const isDoctor = user ? user?.role === 'doctor' : null;
 
   return (
     <UserContextContextProvider user={user} role={user?.role}>
@@ -40,23 +40,24 @@ const App = () => {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/price' element={<RequireAuth access={true}><Price /></RequireAuth>} />
+            <Route path='/price' element={<Price />} />
             <Route path='/sign-up' element={<SignUp />} />
 
             <Route path='/user/me' element={<RequireAuth access={isUser}> <Profile /> </RequireAuth>} />
             <Route path='/profile-edit' element={<RequireAuth access={isUser}> <EditProfile /> </RequireAuth>} />
+            <Route path='/appointments/add' element={<RequireAuth access={isUser}> <AppointmentsAdd /> </RequireAuth>} />
 
-            <Route path='/appointments' element={<RequireAuth> <Appointments /> </RequireAuth>} />
-            <Route path='/appointments/add' element={<RequireAuth> <AppointmentsAdd /> </RequireAuth>} />
-            <Route path='/appointments/edit/:id' element={<RequireAuth> <AppointmentsEdit /> </RequireAuth>} />
-            <Route path='/employee' element={<RequireAuth> <Employee /> </RequireAuth>} />
-            <Route path='/employee/new' element={<RequireAuth> <AddEmployee /> </RequireAuth>} />
-            <Route path='/employee/edit/:id' element={<RequireAuth> <EditEmployee /> </RequireAuth>} />
-            <Route path='/user' element={<RequireAuth> <Users /> </RequireAuth>} />
-            <Route path='/user/new' element={<RequireAuth> <AddNewUser /> </RequireAuth>} />
-            <Route path='/good' element={<RequireAuth> <Goods /> </RequireAuth>} />
-            <Route path='/good/new' element={<RequireAuth> <GoodNew /> </RequireAuth>} />
-            <Route path='/good/edit/:id' element={<RequireAuth> <GoodEdit /> </RequireAuth>} />
+            <Route path='/appointments' element={<RequireAuth access={isUser ||isDoctor}> <Appointments /> </RequireAuth>} />
+            <Route path='/appointments/edit/:id' element={<RequireAuth access={isUser||isDoctor}> <AppointmentsEdit /> </RequireAuth>} />
+
+            <Route path='/user' element={<RequireAuth access={isAdmin}> <Users /> </RequireAuth>} />
+            <Route path='/user/new' element={<RequireAuth access={isAdmin}> <AddNewUser /> </RequireAuth>} />
+            <Route path='/employee' element={<RequireAuth access={isAdmin}> <Employee /> </RequireAuth>} />
+            <Route path='/employee/new' element={<RequireAuth access={isAdmin}> <AddEmployee /> </RequireAuth>} />
+            <Route path='/employee/edit/:id' element={<RequireAuth access={isAdmin}> <EditEmployee /> </RequireAuth>} />
+            <Route path='/good' element={<RequireAuth access={isAdmin}> <Goods /> </RequireAuth>} />
+            <Route path='/good/new' element={<RequireAuth access={isAdmin}> <GoodNew /> </RequireAuth>} />
+            <Route path='/good/edit/:id' element={<RequireAuth access={isAdmin}> <GoodEdit /> </RequireAuth>} />
           </Routes>
         </div>
         <Footer />
