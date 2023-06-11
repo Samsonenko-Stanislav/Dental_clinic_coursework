9e454a34e0d10ec03a19e83106299727afe18c5d
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-11 22:23:56 +0600

    Add Price Page and Update Login Page

--- client/front-end/src/authorization/Login.js
+++ client/front-end/src/authorization/Login.js
@@ -1,53 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import './Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from "axios";
-
+import Tooth from '../tooth.svg';
+import {Link} from 'react-router-dom';
+import axiosApi from "../axiosApi";
 
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-        const submitHandler = async (e)=>{
-                e.preventDefault()
-                try {
-                        const response = await  axios.get('http://localhost:8086/price')
-                        console.log(response);
-                }catch (e) {
-                        console.log(e);
-                }
-        }
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosApi.post('/login' ,{username:email, password});
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
 
-        return (
-            <div className="text-center">
-                    <form  onSubmit={submitHandler}>
-                            <img className="mb-4" src="../tooth.svg" alt="" width="72" height="57" />
-                            <br />
-                            <a href="/">Вернуться на главную</a>
-                            <h1 className="h3 mb-3 fw-normal">Пожалуйста, войдите</h1>
+  return (
+    <div className='text-center container my-2'>
+      <main className='form-signup w-100 m-auto'>
 
-                            <div className={window.location.search.includes('error') ? 'error' : 'hidden'}>
-                                    Неверный логин и/или пароль.
-                            </div>
-                            <div className={window.location.search.includes('logout') ? 'logout' : 'hidden'}>
-                                    Вы вышли.
-                            </div>
+        <form onSubmit={submitHandler}>
+          <img className='mb-4' src={Tooth} alt='' width='72' height='57'/>
+          <br/>
+          <Link to='/'>Вернуться на главную</Link>
+          <h1 className='h3 mb-3 fw-normal'>Пожалуйста, войдите</h1>
 
-                            <div className="form-floating">
-                                    <input type="text" className="form-control" id="floatingInput" placeholder="Логин" name="username" />
-                                    <label htmlFor="floatingInput">Логин</label>
-                            </div>
-                            <div className="form-floating">
-                                    <input type="password" className="form-control" id="floatingPassword" placeholder="Пароль" name="password" />
-                                    <label htmlFor="floatingPassword">Пароль</label>
-                            </div>
+          <div className={window.location.search.includes('error') ? 'error' : 'hidden'}>
+            Неверный логин и/или пароль.
+          </div>
+          <div className={window.location.search.includes('logout') ? 'logout' : 'hidden'}>
+            Вы вышли.
+          </div>
 
-                            <button className="w-100 btn btn-lg btn-primary" type="submit">Войти</button>
-                    </form>
-                    <a href="/sign_up">Зарегистрироваться</a>
-            </div>
-        );
+          <div className='form-floating my-2'>
+            <input type='text' className='form-control' id='floatingInput' placeholder='Логин' name='username'
+                   value={email} onChange={e => setEmail(e.target.value)}/>
+            <label htmlFor='floatingInput'>Логин</label>
+          </div>
+          <div className='form-floating my-2'>
+            <input type='password' className='form-control' id='floatingPassword' placeholder='Пароль'
+                   name='password' value={password} onChange={e => setPassword(e.target.value)}/>
+
+            <label htmlFor='floatingPassword'>Пароль</label>
+          </div>
+
+          <button className='w-100 btn btn-lg btn-primary' type='submit'>Войти</button>
+        </form>
+      </main>
+      <Link to='/sign-up'>Зарегистрироваться</Link>
+    </div>
+  );
 }
 
 export default Login;
---
+++ client/front-end/src/pages/Price.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axiosApi from "../axiosApi";
+
+const Price = () => {
+  const [goods, setGoods] = useState([]);
+
+  useEffect(() => {
+    const fetchGoods = async () => {
+      try {
+        const response = await axiosApi.get('/price');
+        setGoods(response.data?.goods);
+      } catch (error) {
+        console.error('Ошибка при загрузке товаров:', error);
+      }
+    };
+
+    fetchGoods();
+  }, []);
+  return (
+      <>
+        {goods.map((good) => (
+          <div className='col-md-12' key={good.id}>
+            <div
+              className='row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'>
+              <div className='col p-4 d-flex flex-column position-static'>
+                <strong className='d-inline-block mb-2 text-primary'>{good.name}</strong>
+                <div className='mb-1 text-muted'>{good.id}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </>
+  );
+};
+
+
+export default Price;
