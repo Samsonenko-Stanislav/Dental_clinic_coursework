3d85c81c682006ea2c217bded2c894ce85919a8a
Stanislav Samsonenko <stanislav@gmail.com>
2023-06-11 14:43:32 +0600

    Added Config axios

--- client/front-end/src/authorization/Login.js
+++ client/front-end/src/authorization/Login.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import './Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 import Tooth from '../tooth.svg';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import axiosApi from "../axiosApi";
 
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get('http://localhost:8086/price');
+      const response = await axiosApi.post('/login' ,{username:email, password});
       console.log(response);
     } catch (e) {
       console.log(e);
@@ -25,8 +27,8 @@ function Login() {
       <main className='form-signup w-100 m-auto'>
 
         <form onSubmit={submitHandler}>
-          <img className='mb-4' src={Tooth} alt='' width='72' height='57' />
-          <br />
+          <img className='mb-4' src={Tooth} alt='' width='72' height='57'/>
+          <br/>
           <Link to='/'>Вернуться на главную</Link>
           <h1 className='h3 mb-3 fw-normal'>Пожалуйста, войдите</h1>
 
@@ -38,12 +40,14 @@ function Login() {
           </div>
 
           <div className='form-floating my-2'>
-            <input type='text' className='form-control' id='floatingInput' placeholder='Логин' name='username' />
+            <input type='text' className='form-control' id='floatingInput' placeholder='Логин' name='username'
+                   value={email} onChange={e => setEmail(e.target.value)}/>
             <label htmlFor='floatingInput'>Логин</label>
           </div>
           <div className='form-floating my-2'>
             <input type='password' className='form-control' id='floatingPassword' placeholder='Пароль'
-                   name='password' />
+                   name='password' value={password} onChange={e => setPassword(e.target.value)}/>
+
             <label htmlFor='floatingPassword'>Пароль</label>
           </div>
 
