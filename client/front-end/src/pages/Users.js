5b5b2aa5a295d4dc3a045dae5b4ee8e908daff29
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-11 16:48:33 +0600

    Added Permission

--- client/front-end/src/pages/Users.js
+++ client/front-end/src/pages/Users.js
@@ -12,7 +12,7 @@ const Users = () => {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
       const data = await response.json();
-      setUsers(data.map(user=>({...user, roles:'Admin'})));
+      setUsers(data.map((user) => ({ ...user, roles: 'Admin' })));
     } catch (error) {
       console.error('Error fetching users:', error);
     }
@@ -20,41 +20,43 @@ const Users = () => {
 
   return (
     <>
-
-      <div className='table-responsive'>
-        <table className='table table-striped table-sm'>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
           <thead>
-          <tr>
-            <th scope='col'>#</th>
-            <th scope='col'>Логин</th>
-            <th scope='col'>Роли</th>
-            <th scope='col' />
-          </tr>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Логин</th>
+              <th scope="col">Роли</th>
+              <th scope="col" />
+            </tr>
           </thead>
           <tbody>
-          {users.length ? users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td className={user.active ? '' : 'archived'}>{user.username}</td>
-              <td>
-
-                {user.roles}
-                {/*{user.roles.map((role, index) => (*/}
-                {/*  <p key={index}>{role},</p>*/}
-                {/*))}*/}
-              </td>
-              <td>
-                <Link to={`/user/${user.id}`}>Редактировать</Link>
-              </td>
-            </tr>
-          )) : null}
+            {users.length
+              ? users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td className={user.active ? '' : 'archived'}>{user.username}</td>
+                    <td>
+                      {user.roles}
+                      {/*{user.roles.map((role, index) => (*/}
+                      {/*  <p key={index}>{role},</p>*/}
+                      {/*))}*/}
+                    </td>
+                    <td>
+                      <Link to={`/user/${user.id}`}>Редактировать</Link>
+                    </td>
+                  </tr>
+                ))
+              : null}
           </tbody>
         </table>
       </div>
 
-      <Link to='/user/new' className='btn btn-primary mx-2'>Создать нового</Link>
-      <button className='btn btn-primary mx-2'>Показать архивных пользователей</button>
-      <button className='btn btn-primary mx-2'>Скрыть архивных пользователей</button>
+      <Link to="/user/new" className="btn btn-primary mx-2">
+        Создать нового
+      </Link>
+      <button className="btn btn-primary mx-2">Показать архивных пользователей</button>
+      <button className="btn btn-primary mx-2">Скрыть архивных пользователей</button>
     </>
   );
 };
