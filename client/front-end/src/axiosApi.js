638c65a2f3d40724fbc1c2af4e61512bcf0963a3
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-13 00:07:43 +0600

    Update AXios Config

--- client/front-end/src/axiosApi.js
+++ client/front-end/src/axiosApi.js
@@ -1,7 +1,27 @@
 import axios from 'axios';
+import { loadFromLocalStorage } from './utils/localStorage';
+import { logoutUser } from './store/slice/UserSlice';
+import { store } from './index';
 
 const axiosApi = axios.create({
   baseURL: 'http://158.160.43.172:8086',
 });
+
+axiosApi.interceptors.request.use(async (config) => {
+  config.headers = {
+    Authorization: `Bearer ${loadFromLocalStorage('token', false)}`,
+    Accept: 'application/json',
+  };
+  return config;
+});
+
+axiosApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async () => {
+    store.dispatch(logoutUser());
+  }
+);
 
 export default axiosApi;
