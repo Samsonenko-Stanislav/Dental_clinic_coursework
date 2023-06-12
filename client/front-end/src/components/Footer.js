b05822044f0e232c79e420e68d2c891376bead23
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-12 18:00:03 +0600

    Refactoring Code

--- client/front-end/src/components/Footer.js
+++ client/front-end/src/components/Footer.js
@@ -1,18 +1,13 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="mt-auto bg-dark d-flex flex-wrap justify-content-between align-items-center py-3  border-top">
-        <div className="col-md-4 d-flex align-items-center ms-5">
-          <span className="mb-3 mb-md-0 text-muted">
-            © 2023 Самсоненко Станислав
-          </span>
-        </div>
-      </footer>
-    )
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="mt-auto bg-dark d-flex flex-wrap justify-content-between align-items-center py-3  border-top">
+      <div className="col-md-4 d-flex align-items-center ms-5">
+        <span className="mb-3 mb-md-0 text-muted">© 2023 Самсоненко Станислав</span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- client/front-end/src/pages/Home.js
+++ client/front-end/src/pages/Home.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from "../components/Main";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Main_pict from "../main.jpg";
 
 const Home = () => {
-    return (
-        <div>
-            <Main/>
+  return (
+    <div>
+      <div className="content m-5">
+        <p>
+          <b>О стоматологической клинике "Улыбка премиум"</b>
+        </p>
+        <p>
+          В нашей стоматологической клинике работают настоящие профессионалы, которые безболезненно лечат любые стоматологические заболевания с использованием только лучшего
+          оборудования и материалов
+        </p>
+        <p>Мы открыты со вторника по субботу с 8:00 до 21:00</p>
+      </div>
+      <div className="content padding-site m-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-4 text-center">
+              <img alt={'Main_picture'} className="main_pic" src={Main_pict} />
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Home
+export default Home;
