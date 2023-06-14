4422294e96a81e8612ee2f63a4a60878efe5deb5
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-15 03:19:54 +0600

    Update EDIT Appoiment (Doctor)

--- client/front-end/src/pages/AppoimentEdit.js
+++ client/front-end/src/pages/AppoimentEdit.js
@@ -1,7 +1,6 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { UserContext } from '../context/UserContext';
-import axiosApi from '../axiosApi';
-import { editAppointments, getSoloAppointments, nullifyAppointment } from "../store/slice/AppoimentsSlice";
+import { editAppointments, getSoloAppointments, nullifyAppointment } from '../store/slice/AppoimentsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { requestGoods } from '../store/slice/GoodsSlice';
@@ -14,12 +13,29 @@ const AppointmentsEdit = ({ checkLines = [] }) => {
   const params = useParams();
   const { role } = useContext(UserContext);
   const appointmentStore = useSelector((state) => state.appointments.appointment);
-  const appointment = appointmentStore?.appointment || {};
-  const goods = useSelector((state) => state.goods.goods).filter((user) => user.active) || [];
+  const goodsStore = useSelector((state) => state.goods.goods);
   const readOnly = appointmentStore.readOnly;
   const [conclusion, setConclusion] = useState('');
 
+  const [currentCheck, setCurrentCheck] = useState([{ id: 0, good: null, price: null, count: null, total: null }]);
+
   const [total, setTotal] = useState(0);
+
+  const goods = useMemo(() => {
+    return goodsStore.filter((user) => user.active) || [];
+  }, [goodsStore]);
+
+  const appointment = useMemo(() => {
+    return appointmentStore?.appointment || {};
+  }, [appointmentStore]);
+
+  useEffect(() => {
+    const totalSum = currentCheck.reduce((sum, item) => {
+      return sum + (item.total || 0);
+    }, 0);
+
+    setTotal(totalSum);
+  }, [currentCheck]);
 
   useEffect(() => {
     dispatch(getSoloAppointments({ newData: { id: params.id } }));
@@ -39,17 +55,55 @@ const AppointmentsEdit = ({ checkLines = [] }) => {
   }, [dispatch]);
 
   const handleInputChange = (e) => {
-    // Обработчик изменения значения в полях чека
-    // Реализация обработчика зависит от требуемой логики
+    const good = JSON.parse(e.target.value);
+
+    setCurrentCheck((prevState) => {
+      return prevState.map((item) => {
+        if (item.id === good.lineId) {
+          return {
+            ...item,
+            price: good.price,
+            total: good.price * 1,
+            count: 1,
+          };
+        }
+        return item;
+      });
+    });
+  };
+
+  const handlePriceChange = (id, value) => {
+    setCurrentCheck((prevState) => {
+      return prevState.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            count: value,
+            total: value * item.price,
+          };
+        }
+        return item;
+      });
+    });
+  };
+
+  const addFieldHandler = () => {
+    setCurrentCheck((prevState) => {
+      return [...prevState, { id: prevState.length, good: null, price: null, count: null, total: null }];
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    dispatch(editAppointments({newData:{
-        conclusion,
-        id:params.id
-      }}))
+    dispatch(
+      editAppointments({
+        newData: {
+          conclusion,
+          id: params.id,
+        },
+      })
+    );
   };
 
   return (
@@ -104,32 +158,44 @@ const AppointmentsEdit = ({ checkLines = [] }) => {
                         </tr>
                       ))
                     ) : (
-
-
                       <>
-                        <tr>
-                          <td className="w-25">
-                            <select className="form-select good" name="good" onChange={handleInputChange}>
-                              <option value="0" />
-                              {goods.map((good) => (
-                                <option key={good.id} value={good.id}>
-                                  {good.name}
-                                </option>
-                              ))}
-                            </select>
-                          </td>
-                          <td>
-                            <input type="number" disabled step="0.01" name="price" className="form-control price" onChange={handleInputChange} />
-                          </td>
-                          <td>
-                            <input type="number" step="1" name="qty" className="form-control qty" onChange={handleInputChange} />
-                          </td>
-                          <td>
-                            <input type="number" disabled step="0.01" name="total" className="form-control total" onChange={handleInputChange} />
-                          </td>
-                        </tr>
+                        {currentCheck.map((item, index) => {
+                          return (
+                            <tr>
+                              <td className="w-25">
+                                <select className="form-select good" name="good" onChange={handleInputChange}>
+                                  <option value="0" />
+                                  {goods.map((good) => (
+                                    <option key={good.id} value={JSON.stringify({ ...good, lineId: index })}>
+                                      {good.name}
+                                    </option>
+                                  ))}
+                                </select>
+                              </td>
+                              <td>
+                                <input type="number" value={item.price} disabled step="0.01" name="price" className="form-control price" />
+                              </td>
+                              <td>
+                                <input
+                                  type="number"
+                                  value={item.count}
+                                  step="1"
+                                  name="qty"
+                                  min={0}
+                                  className="form-control qty"
+                                  onChange={(e) => handlePriceChange(index, e.target.value)}
+                                />
+                              </td>
+                              <td>
+                                <input type="number" value={item.total} disabled step="0.01" name="total" className="form-control total" />
+                              </td>
+                            </tr>
+                          );
+                        })}
 
-
+                        <button className="btn btn-primary my-2" type={'button'} onClick={addFieldHandler}>
+                          Добавить поле
+                        </button>
 
                         {checkLines.map((checkLine) => (
                           <tr key={checkLine.id}>
