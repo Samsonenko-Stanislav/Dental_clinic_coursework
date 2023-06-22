b3fbe8b9a518532fdc7d47a5ea6988b84bf06120
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-22 19:19:47 +0300

    Fixed a bug when an edit form for a non-existent good was opened

--- client/front-end/src/pages/GoodEdit.js
+++ client/front-end/src/pages/GoodEdit.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { editGoods, getGood, nullifyDataGoods } from '../store/slice/GoodsSlice';
 import { UserContext } from '../context/UserContext';
 import { showNotification } from '../App';
+import NotFound from "./NotFound";
 
 const GoodEdit = () => {
   const navigate = useNavigate();
@@ -45,50 +46,55 @@ const GoodEdit = () => {
     }
     setLoading(false);
   };
-
+if (good.id){
   return (
-    <div className="col-md-7 col-lg-8 mt-4">
-      <h4 className="mb-3">Редактировать услугу</h4>
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-3">
-            <input type="checkbox" name="active" id="active" value={active} checked={active} onChange={(e) => setActive(e.target.checked)} />
-            <label htmlFor="active" className="form-label">
-              Активная
-            </label>
-          </div>
-          <div className="row col-12">
-            <div className="col-6">
-              <label htmlFor="name" className="form-label">
-                Название
+      <div className="col-md-7 col-lg-8 mt-4">
+        <h4 className="mb-3">Редактировать услугу</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-3">
+              <input type="checkbox" name="active" id="active" value={active} checked={active} onChange={(e) => setActive(e.target.checked)} />
+              <label htmlFor="active" className="form-label">
+                Активная
               </label>
-              <input type="text" name="name" className="form-control" id="name" required defaultValue={good.name} value={name} onChange={(e) => setName(e.target.value)} />
             </div>
-            <div className="col-6">
-              <label htmlFor="price" className="form-label">
-                Цена
-              </label>
-              <input
-                type="number"
-                step="0.01"
-                name="price"
-                className="form-control"
-                id="price"
-                required
-                defaultValue={good.price}
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-              />
+            <div className="row col-12">
+              <div className="col-6">
+                <label htmlFor="name" className="form-label">
+                  Название
+                </label>
+                <input type="text" name="name" className="form-control" id="name" required defaultValue={good.name} value={name} onChange={(e) => setName(e.target.value)} />
+              </div>
+              <div className="col-6">
+                <label htmlFor="price" className="form-label">
+                  Цена
+                </label>
+                <input
+                    type="number"
+                    step="0.01"
+                    name="price"
+                    className="form-control"
+                    id="price"
+                    required
+                    defaultValue={good.price}
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
             </div>
+            <input type="hidden" name="goodId" value={good.id} />
+            <button className="w-100 btn btn-primary btn-lg my-4" type="submit">
+              Сохранить
+            </button>
           </div>
-          <input type="hidden" name="goodId" value={good.id} />
-          <button className="w-100 btn btn-primary btn-lg my-4" type="submit">
-            Сохранить
-          </button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
   );
+}
+else {
+  return <NotFound/>
+}
+
 };
 
 export default GoodEdit;
