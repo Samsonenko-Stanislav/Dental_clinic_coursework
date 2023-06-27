1ea8049f53adcaa9a4253b4d7dfbbd544663f203
Stanislav Samsonenko <samsonenko2002@yandex.ru>
2023-06-28 02:01:58 +0300

    Added Postman

--- client/front-end/src/pages/Home.js
+++ client/front-end/src/pages/Home.js
@@ -3,6 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Main_pict from "../assets/main.jpg";
 
 const Home = () => {
+  (function (p, o, s, t, m, a, n
+  ) {
+    !p[s] && (p[s] = function () {
+      (p[t] || (p[t] = [])).push(arguments);
+    });
+    !o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild((
+        (n = o.createElement("script")),
+            (n.id = s+t), (n.async = 1), (n.src = m), n
+    ));
+  }(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"
+  ))
+  ;
   return (
     <div>
       <div className="content m-5">
@@ -23,6 +35,12 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="postman-run-button content padding-site m-3"
+           data-postman-action="collection/fork"
+           data-postman-visibility="private"
+           data-postman-var-1="20983878-56711c53-7817-443a-b940-cb80c5e5b15b"
+           data-postman-collection-url="entityId=20983878-56711c53-7817-443a-b940-cb80c5e5b15b&entityType=collection&workspaceId=63e788f6-1c0b-437d-8635-9e5a5aa63865">
       </div>
     </div>
   );
